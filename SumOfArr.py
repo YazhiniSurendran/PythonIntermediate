6800a060dbddbdd9e8b02fbdca71697bbe4029a9
Yazhini Surendran <30383540+YazhiniSurendran@users.noreply.github.com>
2021-10-11 19:16:50 +0530

    Create SumOfArr.py

---
+++ SumOfArr.py
@@ -0,0 +1,9 @@
+def simpleArraySum(ar):
+    # Write your code here
+    return sum(ar)
+    
+ar_len = int(input("Enter the number of elements in the list "))
+ar = []
+print("Enter the ",ar_len," elements ",sep='')
+for i in range(ar_len):
+  
